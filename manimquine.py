981b920bafb4463e0e2a6463bf0d20394c41011e
Domas Syaifoel <124035858+dmsyaifoel@users.noreply.github.com>
2025-06-23 13:27:33 +0200

    Update manimquine.py

--- manimquine.py
+++ manimquine.py
@@ -3,8 +3,8 @@ s = '''class q(Scene):
     def construct(self):
         r = Code(
             code_string=2*chr(37)+'manim -qm q\\ns ='+3*chr(39)+s[:106]+chr(92)+s[106:166]+chr(92)+s[166]+3*chr(39)+'\\n'+s, 
-            language="python", 
-            paragraph_config=dict(font="Monospace"))
+            language='python', 
+            paragraph_config=dict(font='Monospace'))
         r.scale(.5)
         self.play(Write(r))
         self.wait(10)'''
@@ -12,8 +12,8 @@ class q(Scene):
     def construct(self):
         r = Code(
             code_string=2*chr(37)+'manim -qm q\ns ='+3*chr(39)+s[:106]+chr(92)+s[106:175]+chr(92)+s[175:]+3*chr(39)+'\n'+s, 
-            language="python", 
-            paragraph_config=dict(font="Monospace"))
+            language='python', 
+            paragraph_config=dict(font='Monospace'))
         r.scale(.5)
         self.play(Write(r))
         self.wait(10)
